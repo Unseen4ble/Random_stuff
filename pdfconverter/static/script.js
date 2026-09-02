73d8723a80d82366aaa260f93ba3671ff3f7329e
Unseen4ble <arinjayrc@gmail.com>
2026-09-02 12:02:49 +0530

    updated

--- pdfconverter/static/script.js
+++ pdfconverter/static/script.js
@@ -1,19 +1,32 @@
 const fileInp = document.getElementById('file-Taker');
 const fileList = document.getElementById('file-list');
+const dropArea = document.getElementById('warp');
 
-fileInp.addEventListener('change',()=>{
+fileInp.addEventListener('change',uploadImage)
+
+function uploadImage(){
     fileList.innerHTML='';
 
     const files = fileInp.files;
 
-    if (files.length == 0){
-        fileList.innerHTML='<li>no file selected</li>';
-        return;
-    }
+    // if (files.length == 0){
+    //     fileList.innerHTML='<li>no file selected</li>';
+    //     return;
+    // }
 
     for (let i = 0; i < files.length; i++) {
         const li = document.createElement('li');
         li.textContent = `${files[i].name} (${(files[i].size / 1024).toFixed(1)} KB)`;
         fileList.appendChild(li);
     }
+};
+
+dropArea.addEventListener("drop",(e)=>{
+    e.preventDefault();
+    fileInp.files = e.dataTransfer.files;
+    uploadImage();
+
+});
+dropArea.addEventListener("dragover",(e)=>{
+    e.preventDefault();
 });
